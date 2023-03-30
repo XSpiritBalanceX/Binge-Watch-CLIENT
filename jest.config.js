e9bf0765b34945cbdb015a99658e5a74263610f6
Storm <lenagreidon@mail.ru>
2023-03-30 11:27:01 +0300

    added tests for custom hooks

--- jest.config.js
+++ jest.config.js
@@ -176,7 +176,7 @@ module.exports = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  //transform: {},
+  //transform: {  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   transformIgnorePatterns: ["/node_modules/(?!(ky))"],
